bb4d47e043eb3a4731d28e5b312808e061e28535
simba.wang <simba1986.wang@gmail.com>
2023-08-15 19:44:15 +0800

    [ENH] update docker

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,3 @@
-docker rm -f EPro-PnP-V2
+docker rm -f epro_pnp
 DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name EPro-PnP-V2  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined  -v /home:/home -v /tmp:/tmp -v /mnt:/mnt -v $DIR:$DIR  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE bundlesdf:v1 bash
+xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name epro_pnp  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined  -v /home:/home -v /tmp:/tmp -v /mnt:/mnt -v $DIR:$DIR  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE epro_pnp bash
